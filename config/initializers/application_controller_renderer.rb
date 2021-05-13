24d86e84a0d268d94d7106b8cd05ca87dc594fce
dyoshida1352 <d.yoshida1352@gmail.com>
2021-05-13 08:17:40 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'da1de91dcc3974462f9e52657076277473f441e2094049482dfa823608cbba398d99e5ac7b821a8a7af38b52a9d9c1fe7a7bb8d2814cf0854565fd3e580dcf10'
